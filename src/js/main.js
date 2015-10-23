4f124696409db63a9bafdab27a9b7291774c2eb7
Jon <jmcreasman89@gmail.com>
2015-10-23 12:07:52 -0400

    basic angular working

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,30 @@
+// js for the index page
+// ;(function(){
+//   angular.module('Stack-Undertow', [ ])
+//       .run(function($http, $rootScope){
+//
+//         $http.get('../questions.json')
+//           .then(function (response){
+//             $rootScope.question = response.data;
+//           });
+//
+//       })
+// })(); //END IIFE
+
 ;(function(){
+  // angular.module('Stack-Undertow', [ ])
+  //   .run(function($http, $rootScope){
+  //     $http.get('../questions.json')
+  //     .then(function (response){
+  //       console.log(arguments);
+  //     })
+  //     });
   angular.module('Stack-Undertow', [ ])
-      .run(function($http, $rootScope){
-
-        $http.get('../questions.json')
-          .then(function (response){
-            $rootScope.questions = response.data;
+    .run(function($http, $rootScope){
+      $http.get('../questions.json')
+        .then(function (response){
+          console.log(arguments);
+          $rootScope.question = response.data[0];
           });
-
-      })
+        })
 })(); //END IIFE
